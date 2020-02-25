f4dc286471ccd320c9d5fbfca8a3d64f3dd356dd
Pan Artur <artur.pankov.16@list.ru>
2020-02-25 23:51:44 +0300

    feat: add task resolution

--- src/index.js
+++ src/index.js
@@ -3,15 +3,59 @@
 
 module.exports = function towelSort (matrix) {
 
-  let res = [];
-
-  matrix.forEach((it) => {
-
-    for (let i = 0; i < it.length; i++) {
-      res.push(it[i]);
+  if (Array.isArray(matrix) && matrix.length !== 0) {
+    let bubleSortToHight = (sortingArr) => {
+      for (let i = 1; i < sortingArr.length; i++) {
+          for (let j = 0; j < sortingArr.length; j++) {
+              if (sortingArr[j] < sortingArr[i]) {
+                let swap = sortingArr[j];
+                sortingArr[j] = sortingArr[i];
+                sortingArr[i] = swap;
+              } 
+            
+        }
+        
+    
     }
+    }
+  
+    let bubleSortToLow = (sortingArr) => {
+      for (let i = 1; i < sortingArr.length; i++) {
+  
+        if (typeof sortingArr[i] === 'number') {
+        for (let j = 0; j < sortingArr.length; j++) {
+          if (sortingArr[j] > sortingArr[i]) {
+            let swap = sortingArr[j];
+            sortingArr[j] = sortingArr[i];
+            sortingArr[i] = swap;
+          } 
+      }
+      } else {
+        console.log(sortingArr[i]);
+      }
+    }
+    }
+  
+    bubleSortToLow(matrix[0]);
 
-  });
+    let finalArr = [];
 
-  return res;
+    for (let l = 1; l < matrix.length; l += 2) {
+   
+      bubleSortToHight(matrix[l]);
+    }
+  
+    matrix.forEach( (it) => {
+      
+      for (let i = 0; i < it.length; i++) {
+        finalArr.push(it[i]);
+      }
+  
+    });
+  
+    return finalArr;
+  } else {
+    return [];
+  } 
+
 }
